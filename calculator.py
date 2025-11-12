d7b09ed8502c1742dae174af1ce806ca8fd4bbb7
mpaquette1-svg <mpaquette1@ufl.edu>
2025-11-12 13:37:32 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -1,11 +1,40 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/mpaquette1-svg/lab11-MP-MEG.git
+#Partner 1: Matthew Paquette
+#Partner 2: Miguel Gutierrez
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
 
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    else:
+        return math.sqrt(a)
+
+def hypotenuse(a,b):
+    return math.hypot(a, b)
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a != 0:
+        return b / a # raise ZeroDivisionError if a == 0
+    else:
+        raise ZeroDivisionError
+
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    else:
+        return math.log(a,b)# use math library + raise ValueError
+
+def exp(a, b):
+    return a**b
 
