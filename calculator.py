4c1b3e75033193924aa33fa3deca4c28d6ddc830
Miguel Gutierrez <meguti04@gmail.com>
2025-11-12 12:57:43 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,31 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+#https://github.com/mpaquette1-svg/lab11-MP-MEG.git
+# task 1 - MP
+# task 2 - MEG
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        a / b
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(a,b) # use math library + raise ValueError
+
+def exp(a, b):
+    return a ** b
+
 
 
