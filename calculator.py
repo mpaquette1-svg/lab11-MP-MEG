6cd60986affcb5d14633aadeb9230f277765557e
mpaquette1-svg <mpaquette1@ufl.edu>
2025-11-13 17:48:43 -0500

    changes

--- calculator.py
+++ calculator.py
@@ -16,7 +16,7 @@ def hypotenuse(a,b):
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
 def mul(a, b):
@@ -29,7 +29,7 @@ def div(a, b):
         raise ZeroDivisionError
 
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError
     else:
